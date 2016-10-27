d944718bb25fd30e420ccdd3cc878c57024301fe
Atle Frenvik Sveen <atle.sveen@norkart.no>
2016-10-27 13:09:44 +0200

    use equal area for d3

--- src/d3/index.js
+++ src/d3/index.js
@@ -44,9 +44,9 @@ var data = topojson.feature(kommuner, kommuner.objects.kommuner);
 
 
 // create a unit projection
-var projection = d3.geoTransverseMercator()
+var projection = d3.geoAzimuthalEqualArea()
     .scale(1)
-    .rotate([-15, 0])
+    .rotate([-10, 0])
     /*.parallels([50, 70])
     .center([10, 60])
     .rotate([-15, 0])*/
